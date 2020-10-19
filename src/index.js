bb60cebb7b792393c141acf1cd7fb4a45ae969c7
Fialkaksu <fialkaksu1984@gmail.com>
2020-10-19 18:30:10 +0300

    feat: completed task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    if (str.length % 2 !== 0) {
+        return false;
+    }
+
+    repeatCheck();
+
+    function repeatCheck() {
+        bracketsConfig.forEach(el => {
+            el = el.join('');
+            if (str.includes(el)) {
+                str = str.replace(el, '');
+                repeatCheck();
+            }
+        });
+    }
+
+    return str.length === 0;
 }
